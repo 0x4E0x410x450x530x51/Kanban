9a1d21eb71dba5f252f6d40d0927c1a4c61f45de
Cupbwirb <96288920+Cupbwirb@users.noreply.github.com>
2022-06-15 16:26:10 +0200

    Dashboard no progress
    Set previous code into commentary and tried to make it into a grid which adapts to the window size.

--- project/src/main/resources/static/scripts/dashboard_script.js
+++ project/src/main/resources/static/scripts/dashboard_script.js
@@ -8,3 +8,43 @@ function closeNav() { 
     document.getElementById("mySidenav").style.width = "0"; 
     document.getElementById("main").style.marginLeft = "0";
 }
+
+
+// New Add
+const BtnAdd = document.querySelector(".btn-add");
+const DivContainer = document.getElementById("div-container");
+
+function addNew() {
+    var todo = document.getElementById("div-container");
+    var taskName = document.getElementById("newDiv").value;
+  
+    todo.innerHTML += `
+  
+    <div class="task" id="${taskName.toLowerCase().split(" ").join("")}">
+  
+        <span>${taskName}</span>
+  
+    </div>`
+}
+function saveTask() {
+
+    var todo = document.getElementById("todo");
+  
+    var taskName = document.getElementById("task-name").value;
+  
+  
+  
+    if (taskName == 0) alert('You have to name the story.') // checks if the story is named
+  
+    else {
+  
+      todo.innerHTML += `
+  
+    <div class="task" id="${taskName.toLowerCase().split(" ").join("")}" draggable="true" ondragstart="drag(event)">
+  
+        <span>${taskName}</span>
+  
+    </div>`
+      createTask()
+  }
+}
