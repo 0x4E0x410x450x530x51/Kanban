ea378e7868b887ef6e6ed4d5a36c8fb78c65cb39
QueSchoenenberger <schoenenbergerq@bzz.ch>
2022-06-15 13:55:51 +0200

    Buggfixes
    Prevented the storys from dropping into other storys

--- project/src/main/resources/static/scripts/kanbanboard.js
+++ project/src/main/resources/static/scripts/kanbanboard.js
@@ -24,4 +24,51 @@ function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
+  ev.currentTarget.appendChild(document.getElementById(data)); // prevents the storys from droping in an other story
 }
+
+/* Creates a new Story */
+function createTask() {
+  var x = document.getElementById("inprogress");
+  var y = document.getElementById("done");
+  var z = document.getElementById("create-new-task-block");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "block";
+    z.style.display = "none";
+  } else {
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "flex";
+  }
+}
+
+/* Saves the story in the Backlog */
+function saveTask() {
+  var todo = document.getElementById("todo");
+  var taskName = document.getElementById("task-name").value;
+
+  if (taskName == 0) alert('You have to name the story.') // checks if the story is named
+  else {
+    todo.innerHTML += `
+  <div class="task" id="${taskName.toLowerCase().split(" ").join("")}" draggable="true" ondragstart="drag(event)">
+      <span>${taskName}</span>
+  </div>
+  `
+
+    createTask()
+  }
+
+}
+/*
+function editTask(){
+  var saveButton = document.getElementById("save-button");
+  var editButton = document.getElementById("edit-button");
+  if (saveButton.style.display === "none") {
+      saveButton.style.display = "block";
+      editButton.style.display = "none";
+  } else{
+      saveButton.style.display = "none";
+      editButton.style.display = "block";
+  }
+}*/
