b91aa191a82785ae179177cfbc56611e4f9f64db
QueSchoenenberger <schoenenbergerq@bzz.ch>
2022-06-15 11:44:33 +0200

    Kanbanboard
    CSS, HTML, JS, Bootstrap

---
+++ project/src/main/resources/static/scripts/kanbanboard.js
@@ -0,0 +1,13 @@
+
+
+// Opens the sidebar on the Kanban Board site.
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+// Closes the sidebar on the Kanban Board site.
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
