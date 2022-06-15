4c1a88bdbe882c97d6b9a905d02fa9a9e6aabdd5
QueSchoenenberger <schoenenbergerq@bzz.ch>
2022-06-15 13:11:17 +0200

    Drag and Drop
    You can drag and drop storys now.

--- project/src/main/resources/static/scripts/kanbanboard.js
+++ project/src/main/resources/static/scripts/kanbanboard.js
@@ -1,5 +1,4 @@
 
-
 // Opens the sidebar on the Kanban Board site.
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -10,4 +9,19 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+}
+
+/* Drag and drop storys in the Kanban Board */
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
 }
