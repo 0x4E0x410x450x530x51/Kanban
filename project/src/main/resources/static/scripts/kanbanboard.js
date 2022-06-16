156391b786bbce0cbbc0ec8cf1f638fd23ef318d
AndyI0I <92602614+AndyI0I@users.noreply.github.com>
2022-06-16 13:48:47 +0200

    border stuff
    added that if you drag a story element that a border appears

--- project/src/main/resources/static/scripts/kanbanboard.js
+++ project/src/main/resources/static/scripts/kanbanboard.js
@@ -31,6 +31,11 @@ function manageSidebar() {
 /* Drag and drop storys in the Kanban Board */
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
+  console.log(1)
+  var elements = document.getElementsByClassName("story-table")
+  for(i = 0; i < elements.length; i++){
+    elements[i].classList.add("story-border")
+  }
 }
 
 function allowDrop(ev) {
@@ -39,9 +44,15 @@ function allowDrop(ev) {
 
 function drop(ev) {
   ev.preventDefault();
+    console.log(1)
+
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
   ev.currentTarget.appendChild(document.getElementById(data)); // prevents the storys from droping in an other story
+  var elements = document.getElementsByClassName("story-table")
+  for(i = 0; i < elements.length; i++){
+    elements[i].classList.remove("story-border")
+  }
 }
 
 /* Creates a new Story */
