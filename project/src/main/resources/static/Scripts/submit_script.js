6cefab0eff279f8bafcad9b5503b2121d844dc9a
Cupbwirb <96288920+Cupbwirb@users.noreply.github.com>
2022-07-12 13:47:02 +0200

    Submit Site
    Done by my site

---
+++ project/src/main/resources/static/Scripts/submit_script.js
@@ -0,0 +1,37 @@
+// Opens the sidebar on the Kanban Board site.
+var navState = 0
+
+function toggleNav() {
+    if (navState == 0) {
+        openNav()
+        navState = 1
+    } else {
+        closeNav()
+        navState = 0
+    }
+}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("tlines").style.marginLeft = "250px";
+}
+// Closes the sidebar on the Kanban Board site.
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("tlines").style.marginLeft = "";
+}
+
+
+
+function addFunction() {
+    const placeToPut = document.getElementById("submitTable")
+    placeToPut.innerHTML += `
+                <tr>
+                    <td>Test data</td>
+                    <td>Test data</td>
+                    <td>Test data</td>
+                    <td><button type="button" class="btn btn-danger">View</button></td>
+                </tr>
+    `
+}
